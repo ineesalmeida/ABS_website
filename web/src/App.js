dec4fed598bfb06ac6ee4ded84c9dab4479fba63
Ines <ineesalmeida@gmail.com>
2021-03-09 10:35:47 +0100

    add react app

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Logo from './components/Logo/Logo.component';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Logo />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/components/Logo/Logo.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Logo.style.css';
+
+function Logo() {
+    return (
+        <div className="logo">
+            <h1>Australian Barbecue Survival</h1>
+        </div>
+    );
+}
+
+export default Logo;
