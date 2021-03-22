2c0a7848918a46cb2069af94fb8ebd5613659724
Ines <ineesalmeida@gmail.com>
2021-03-22 23:29:54 +0100

    add social media

---
+++ web/src/components/SocialMedia/SocialMedia.component.js
@@ -0,0 +1,21 @@
+import './SocialMedia.style.css';
+
+export function SocialMediaItem({ keyword, icon, link }) {
+    return (
+        <div className="social__icon">
+            <a href={link} target="_blank" rel="noreferrer">
+                <i className={icon} title={keyword} aria-label={"Go to " + keyword} />
+            </a>
+        </div>
+    )
+}
+
+export default function SocialMedia({ social_media_items }) {
+    return (
+        <div className="social__container">
+            {social_media_items.map(s =>
+                <SocialMediaItem key={s.keyword} {...s} />
+            )}
+        </div>
+    )
+}
