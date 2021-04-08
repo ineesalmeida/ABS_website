ca3efab9055f33aca2c307be37dcbdf92bdb4aba
Ines <ineesalmeida@gmail.com>
2021-04-08 10:38:08 +0200

    update router

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar.component';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 import MainPage from './pages/Main';
 import AboutPage from './pages/About';
 import PolicyPage from './pages/Policy';
@@ -11,14 +11,17 @@ function App() {
       <Navbar />
       <div className="body_container">
         <Switch>
+          <Route exact path="/">
+            <MainPage />
+          </Route>
           <Route path="/about">
             <AboutPage />
           </Route>
           <Route path="/policy">
             <PolicyPage />
           </Route>
-          <Route path="/">
-            <MainPage />
+          <Route exact path="*">
+            <Redirect to="/" />
           </Route>
         </Switch>
       </div>
